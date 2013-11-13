14452b52b65700af5edc653e05c220cc33379dc4
Gino Gervasio <ginogervasio@gmail.com>
2013-11-12 20:28:39 -0800

    Updated page layout and content

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, jsonify, Markup, render_template
+from flask import Flask, jsonify, Markup, render_template, request
 
 from salvavida.database import db_session, init_db
 from salvavida.svmodels import Feed
@@ -9,8 +9,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    feeds = [i.serialize for i in Feed.query.all()]
+    feeds = [i.serialize for i in Feed.query.filter(Feed.tag=='sos').all()]
     return render_template('map.html', feeds=Markup(feeds))
+
+@app.route("/rescue", methods=["POST", "GET"])
+@app.route("/sos", methods=["POST", "GET"])
+def sos():
+    return jsonify(json.loads(request.data))
 
 if __name__ == '__main__':
     app.debug = True
